834cdc9057b9e71cdd292aa8285e1c0ec7b5c918
thing852 <hcshin@pnpsecure.com>
2021-07-06 16:17:14 +0900

    d

--- TC Allow Test_CentOS7.8/src/fac_def.py
+++ TC Allow Test_CentOS7.8/src/fac_def.py
@@ -84,7 +84,7 @@ def logCheck(tc_num):
 	result = ''
 
         # ACT:'ALLOW/DENY' PNM:'Policy_Name'
-	log_file = os.popen('cat /etc/.pfcpath').read()
+	log_file = os.popen('cat /etc/.pfcpath').read().split('.')[1]
 	log_check = os.popen("tail -1 %s/log/pfclog | awk '{print $6, $22}'" %log_file).read()
 	tmp = re.split('[: ]+',log_check)
 	print(tmp)	
