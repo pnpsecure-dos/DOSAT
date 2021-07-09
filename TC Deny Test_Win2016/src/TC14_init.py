37fce42ff3f1f9537bbebadd5d523834f5a415f1
thing852 <hcshin@pnpsecure.com>
2021-07-09 11:24:11 +0900

    테이블 데이터 삭제 쿼리 변경(truncate -> delete)

--- TC Deny Test_Win2016/src/TC14_init.py
+++ TC Deny Test_Win2016/src/TC14_init.py
@@ -9,19 +9,19 @@ dt = nowDate()
 # change policy on/off depending on variables.policy_status
 if policy_status == "ALLOW":
 	dbExecute("dbsafer3","update %s set enabled=1 where name like 'TC%%';"%(tb_fac))
-	dbExecute("dbsafer_log_%s_%s"%(dt['year'], dt['month']),"truncate access_file_%s;"%dt['day'])
+	dbExecute("dbsafer_log_%s_%s"%(dt['year'], dt['month']),"delete from access_file_%s;"%dt['day'])
 	
 	dbExecute("dbsafer3","update %s set enabled=1 where name like 'TC%%';"%(tb_tcp_ctrl))
-	dbExecute("dbsafer_log_%s_%s"%(dt['year'], dt['month']),"truncate etc_acl_%s;"%dt['day'])
+	dbExecute("dbsafer_log_%s_%s"%(dt['year'], dt['month']),"delete from etc_acl_%s;"%dt['day'])
 
 elif policy_status == "DENY":
 	dbExecute("dbsafer3", "update %s set enabled=0 where name like 'TC%%';"%(tb_fac))
 	dbExecute("dbsafer3", "update %s set enabled=1 where name like 'TC%%' and name like '%%deny';"%(tb_fac))
-	dbExecute("dbsafer_log_%s_%s"%(dt['year'], dt['month']),"truncate access_file_%s;"%dt['day'])
+	dbExecute("dbsafer_log_%s_%s"%(dt['year'], dt['month']),"delete from access_file_%s;"%dt['day'])
 
 	dbExecute("dbsafer3", "update %s set enabled=0 where name like 'TC%%';"%(tb_tcp_ctrl))
 	dbExecute("dbsafer3", "update %s set enabled=1 where name like 'TC%%' and name like '%%deny';"%(tb_tcp_ctrl))
-	dbExecute("dbsafer_log_%s_%s"%(dt['year'], dt['month']),"truncate etc_acl_%s;"%dt['day'])
+	dbExecute("dbsafer_log_%s_%s"%(dt['year'], dt['month']),"delete from etc_acl_%s;"%dt['day'])
 
 #send to server_manager
 usock = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
