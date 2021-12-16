d4cb7df08bf80fedcadf51fb631526fcedd9483b
thing852 <hcshin@pnpsecure.com>
2021-12-16 10:11:53 +0900

    예외 정책 TC 추가

---
+++ TC Allow Test/src/TC14_40_1_create.py
@@ -0,0 +1,31 @@
+import os,sys, platform
+from time import sleep
+from fac_def import *
+from variables import *
+
+os_platform = platform.system()
+tc_num = os.path.basename(__file__).split('.')[0]
+
+if os_platform == "Windows" :
+    path = "C:\\fac_test_dir\\%s"%tc_num
+else :
+    path = "/home/fac_test_dir/%s"%tc_num
+
+if os.path.isfile(path):
+	os.remove(path)
+
+try:
+	f = open(path, 'w')
+	f.write("TC14_40_1_create test file")
+	f.close()
+except Exception as e:
+	print(e)
+	
+sleep(1)
+
+if logCheck(tc_num, os_platform) == policy_status :
+	print("true")
+	sys.exit(0)
+else:
+	print("fail")
+	sys.exit(-1)
