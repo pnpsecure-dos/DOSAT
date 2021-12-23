c7d4493b33560c816d97165d6cbef80783cf2daa
thing852 <hcshin@pnpsecure.com>
2021-12-23 15:51:07 +0900

    제어유형 TC 활성화

--- TC Allow Test/TC_test.py
+++ TC Allow Test/TC_test.py
@@ -27,37 +27,37 @@ class TC_test(unittest.TestCase):
         rtn = os.system('python ./src/TC14_11.py')
         self.assertEqual(rtn,0)
 
-    # def test_TC14_12_1_create(self):
-    #     rtn = os.system('python ./src/TC14_12_1_create.py')
-    #     self.assertEqual(rtn,0)
+    def test_TC14_12_1_create(self):
+        rtn = os.system('python ./src/TC14_12_1_create.py')
+        self.assertEqual(rtn,0)
 
-    # def test_TC14_12_2_write(self):
-    #     rtn = os.system('python ./src/TC14_12_2_write.py')
-    #     self.assertEqual(rtn,0)
+    def test_TC14_12_2_write(self):
+        rtn = os.system('python ./src/TC14_12_2_write.py')
+        self.assertEqual(rtn,0)
 
-    # def test_TC14_12_3_read(self):
-    #     rtn = os.system('python ./src/TC14_12_3_read.py')
-    #     self.assertEqual(rtn,0)
+    def test_TC14_12_3_read(self):
+        rtn = os.system('python ./src/TC14_12_3_read.py')
+        self.assertEqual(rtn,0)
 
-    # def test_TC14_12_4_rename(self):
-    #     rtn = os.system('python ./src/TC14_12_4_rename.py')
-    #     self.assertEqual(rtn,0)
+    def test_TC14_12_4_rename(self):
+        rtn = os.system('python ./src/TC14_12_4_rename.py')
+        self.assertEqual(rtn,0)
 
-    # def test_TC14_12_5_delete(self):
-    #     rtn = os.system('python ./src/TC14_12_5_delete.py')
-    #     self.assertEqual(rtn,0)
+    def test_TC14_12_5_delete(self):
+        rtn = os.system('python ./src/TC14_12_5_delete.py')
+        self.assertEqual(rtn,0)
 
-    # def test_TC14_12_6_execute(self):
-    #     rtn = os.system('python ./src/TC14_12_6_execute.py')
-    #     self.assertEqual(rtn,0)
+    def test_TC14_12_6_execute(self):
+        rtn = os.system('python ./src/TC14_12_6_execute.py')
+        self.assertEqual(rtn,0)
 
-    # def test_TC14_12_7_mkdir(self):
-    #     rtn = os.system('python ./src/TC14_12_7_mkdir.py')
-    #     self.assertEqual(rtn,0)
+    def test_TC14_12_7_mkdir(self):
+        rtn = os.system('python ./src/TC14_12_7_mkdir.py')
+        self.assertEqual(rtn,0)
 
-    # def test_TC14_12_8_rmdir(self):
-    #     rtn = os.system('python ./src/TC14_12_8_rmdir.py')
-    #     self.assertEqual(rtn,0)
+    def test_TC14_12_8_rmdir(self):
+        rtn = os.system('python ./src/TC14_12_8_rmdir.py')
+        self.assertEqual(rtn,0)
 
     def test_TC14_21(self):
         rtn = os.system('python ./src/TC14_21.py')
