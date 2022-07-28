f4b43d7c9c6c0dd0dd14ec2197d4b193dff81b64
thing852 <hcshin@pnpsecure.com>
2022-07-28 15:25:52 +0900

    upd 제어에서 deny 된 경우 예외 처리

--- TC_Test/src/udp_server.py
+++ TC_Test/src/udp_server.py
@@ -9,17 +9,18 @@ bufferSize  = 1024
 send_msg = str.encode("Hello UDP Client")
 server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 server_socket.bind((host, port))
+server_socket.settimeout(10)
 
 print("UDP server up")
 
-msg_from_client = server_socket.recvfrom(bufferSize)
-message = msg_from_client[0]
-address = msg_from_client[1]
-
-client_msg = "Message from Client:{}".format(message)
-client_ip  = "Client IP Address:{}".format(address)
-
-print(client_msg)
-print(client_ip)
-
-server_socket.sendto(send_msg, address)
+try:
+    msg_from_client = server_socket.recvfrom(bufferSize)
+    message = msg_from_client[0]
+    address = msg_from_client[1]
+    client_msg = "Message from Client:{}".format(message)
+    client_ip  = "Client IP Address:{}".format(address)
+    print(client_msg)
+    print(client_ip)
+except socket.timeout:
+    print("time out")
+    pass
