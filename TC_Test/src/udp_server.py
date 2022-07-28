a4d49c310591c827d6b8f7a078df69dc1e453819
thing852 <hcshin@pnpsecure.com>
2022-07-28 16:17:52 +0900

    upd server 예외처리 추가

--- TC_Test/src/udp_server.py
+++ TC_Test/src/udp_server.py
@@ -8,22 +8,25 @@ port = int(sys.argv[1])
 bufferSize  = 1024
 
 send_msg = str.encode("Hello UDP Client")
-server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-server_socket.bind((host, port))
-server_socket.settimeout(10)
+udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+udp_server_socket.bind((host, port))
+udp_server_socket.settimeout(30)
 
 print("UDP server up")
 
 try:
-    msg_from_client = server_socket.recvfrom(bufferSize)
+    msg_from_client = udp_server_socket.recvfrom(bufferSize)
     message = msg_from_client[0]
     address = msg_from_client[1]
     client_msg = "Message from Client:{}".format(message)
     client_ip  = "Client IP Address:{}".format(address)
     print(client_msg)
     print(client_ip)
+
+except KeyboardInterrupt:
+    raise KeyboardInterrupt
+
 except socket.timeout:
-    print("time out")
     pass
 
-server_socket.close()
+udp_server_socket.close()
