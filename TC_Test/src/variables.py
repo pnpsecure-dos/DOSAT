2135e186d66e4fd536b12c7bfe27e73bea8584ec
thing852 <hcshin@pnpsecure.com>
2022-08-04 11:54:08 +0900

    jenkins 에서 문자 입력을 위한 공백 추가

--- TC_Test/src/variables.py
+++ TC_Test/src/variables.py
@@ -5,3 +5,4 @@ svr_ip = '192.168.105.65'
 log_tables = ["access_file", "etc_acl", "access_process", "bind_control", "setuid_file"]
 policy_tables = ["policy_file_auth", "policy_process_kill_auth", "policy_setuid_auth",
                 "policy_pb_auth", "policy_hf_auth"]
+
