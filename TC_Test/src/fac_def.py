ea32b8739b50eaead0fd14e69c94c671c8a40d1e
thing852 <hcshin@pnpsecure.com>
2023-07-26 10:18:27 +0900

    db update 후 commit 추가

--- TC_Test/src/fac_def.py
+++ TC_Test/src/fac_def.py
@@ -7,67 +7,68 @@ import os, sys
 from datetime import datetime
 
 def runOnShell(client, cmdlines):
-	"""command to client server use paramiko 
-	Args: 
-		client(paramiko.SSHco):
-		cmdlines(list):
-	Return:
-		dict_ret = {
-			'cmd':cmdlines, 
-			'recv':[]
-		}
-	Raises:
-	"""
-	sh = client.invoke_shell()
-	dict_ret = {'cmd':cmdlines, 'recv':[]}
-	for cmd in cmdlines:
-		ret = sh.send(cmd+"\n")
-		time.sleep(0.5)
-		buf=''
-		while sh.recv_ready():
-			buf = sh.recv(4096)
-			dict_ret['recv'].append(re.split('[\r\n]+', buf.decode('utf-8'))[3:-1])
-	sh.close()
-	return dict_ret
+    """command to client server use paramiko 
+    Args: 
+        client(paramiko.SSHco):
+        cmdlines(list):
+    Return:
+        dict_ret = {
+            'cmd':cmdlines, 
+            'recv':[]
+        }
+    Raises:
+    """
+    sh = client.invoke_shell()
+    dict_ret = {'cmd':cmdlines, 'recv':[]}
+    for cmd in cmdlines:
+        ret = sh.send(cmd+"\n")
+        time.sleep(0.5)
+        buf=''
+        while sh.recv_ready():
+            buf = sh.recv(4096)
+            dict_ret['recv'].append(re.split('[\r\n]+', buf.decode('utf-8'))[3:-1])
+    sh.close()
+    return dict_ret
 
 def dbExecute(db,sql):
-	"""db connect, execute sql and db close
-	Args:
-		db(str): db what user use
-		sql(str): sql command
-	Return: X
-	Raise:
-	
-	"""
-	con = py.connect(host=svr_ip, user= db_usr, password = db_pwd, db=db, charset='utf8')
-	cur = con.cursor()
-	cur.execute(sql)
-	if sql.split(' ')[0] == 'select':
-		rows = cur.fetchall()
-		con.close()
-		return rows
-	con.close()
+    """db connect, execute sql and db close
+    Args:
+        db(str): db what user use
+        sql(str): sql command
+    Return: X
+    Raise:
+    
+    """
+    con = py.connect(host=svr_ip, user= db_usr, password = db_pwd, db=db, charset='utf8')
+    cur = con.cursor()
+    cur.execute(sql)
+    if sql.split(' ')[0] == 'select':
+        rows = cur.fetchall()
+        con.close()
+        return rows
+    con.commit()
+    con.close()
 
 def nowDate():
-	"""Now date, time 
-	Args: X
-	Return :	
-		dt_dict ={
-			"year":dt[0],
-			"month":dt[1],
-			"day":dt[2],
-			"time":dt[3]
-		}
-	Reises:
-	"""
-	dt = re.split('[-T]+',datetime.now().isoformat())
-	dt_dict ={
-		"year":dt[0],
-		"month":dt[1],
-		"day":dt[2],
-		"time":dt[3]
-	}
-	return dt_dict
+    """Now date, time 
+    Args: X
+    Return :	
+        dt_dict ={
+            "year":dt[0],
+            "month":dt[1],
+            "day":dt[2],
+            "time":dt[3]
+        }
+    Reises:
+    """
+    dt = re.split('[-T]+',datetime.now().isoformat())
+    dt_dict ={
+        "year":dt[0],
+        "month":dt[1],
+        "day":dt[2],
+        "time":dt[3]
+    }
+    return dt_dict
 
 
 
