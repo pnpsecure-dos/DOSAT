245a7192676c8b081854d59f96a51476c5b13972
thing852 <hcshin@pnpsecure.com>
2023-08-01 09:42:45 +0900

    teardown 에서 정책 변경 시그널 추가

--- TC_Test/src/TC14_teardown.py
+++ TC_Test/src/TC14_teardown.py
@@ -1,6 +1,11 @@
 from fac_def import *
 from variables import *
+import socket as skt
 
 # change policy on/off depending on variables.policy_status
 for policy_table in policy_tables:
     dbExecute("dbsafer3","update %s set enabled=0 where name like 'TC%%';"%policy_table)
+
+# send to server_manager
+usock = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
+usock.sendto('.'.encode(), (svr_ip,21114))
